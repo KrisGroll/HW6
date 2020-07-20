8e262800fb385021a5f96a7428f7d1adf9fce94e
KrisGroll <Kristina-gorelikova@mail.ru>
2020-07-20 17:22:08 +0200

    Initial Commit

---
+++ HW6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW6
+//
+//  Created by KrisGroll on 20.07.2020.
+//  Copyright © 2020 KrisGroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
